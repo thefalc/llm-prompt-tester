14a715b6d2bbe0df2174ff4b94ab29d7e8407fd8
thefalc <falconer.sean@gmail.com>
2025-02-11 14:14:18 -0800

    Removed the auth screen

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
-import { useState } from 'react';
 import Layout from '../components/Layout';
 import ChatApp from '../components/ChatApp';
-import AuthScreen from '../components/AuthScreen';
 
 export default function Index() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   return (
     <Layout title="Nimbus Prompter 2000">
-      {isAuthenticated ? (
-        <ChatApp />
-      ) : (
-        <AuthScreen onAuthenticate={() => setIsAuthenticated(true)} />
-      )}
+      <ChatApp />
     </Layout>
   );
 }
